d95b8c52f34f41e3400e8c777ef37742a15e6a5d
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-06 14:14:51 +0800

    concurrent event transformer

--- lib/blocs/counter/counter_bloc.dart
+++ lib/blocs/counter/counter_bloc.dart
@@ -1,5 +1,6 @@
 // ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 
 part 'counter_event.dart';
@@ -8,20 +9,25 @@ part 'counter_state.dart';
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterState.initial()) {
     on<IncrementCounterEvent>(_incrementCounter);
-    on<DecrementCounterEvent>(_decrementCounter);
+    on<DecrementCounterEvent>(
+      _decrementCounter,
+      transformer: concurrent(), // default
+    );
   }
 
-  void _incrementCounter(
+  Future<void> _incrementCounter(
     IncrementCounterEvent event,
     Emitter<CounterState> emit,
-  ) {
+  ) async {
+    await Future.delayed(const Duration(seconds: 4));
     emit(state.copyWith(counter: state.counter + 1));
   }
 
-  void _decrementCounter(
+  Future<void> _decrementCounter(
     DecrementCounterEvent event,
     Emitter<CounterState> emit,
-  ) {
+  ) async {
+    await Future.delayed(const Duration(seconds: 2));
     emit(state.copyWith(counter: state.counter - 1));
   }
 }
