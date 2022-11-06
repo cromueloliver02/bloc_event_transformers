697fd02f8827388ea8e66569b703ff1d3a49f8e3
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-06 13:43:28 +0800

    project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'blocs/counter/counter_bloc.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,7 +14,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: BlocProvider<CounterBloc>(
+        create: (ctx) => CounterBloc(),
+        child: const HomePage(),
+      ),
     );
   }
 }
@@ -22,9 +27,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(title: const Text('BLOC EVENT TRANSFORMER')),
       body: Center(
-        child: Text('EVENT BLOC TRANSFORMER'),
+        child: BlocSelector<CounterBloc, CounterState, int>(
+          selector: (state) => state.counter,
+          builder: (ctx, counter) => Text(
+            '$counter',
+            style: const TextStyle(fontSize: 52),
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () =>
+                context.read<CounterBloc>().add(IncrementCounterEvent()),
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: () =>
+                context.read<CounterBloc>().add(DecrementCounterEvent()),
+            child: const Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
